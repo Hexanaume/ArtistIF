69aa1fb960c3ed5470f6bda247a5009703be264b
adrienmorin1 <adridu71170@gmail.com>
2023-12-06 10:32:54 +0100

    modify name

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,9 +1,9 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'ArtistIF',
+  author: 'Hexanome 4123',
+  headerTitle: 'ArtistIF',
+  description: 'An artistIF website',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
