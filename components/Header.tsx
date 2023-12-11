d7a80f0bd4150f392e12ea0b9dcc6cc945f266af
Laziza Bellahbib <aziza.bellahbib@insa-lyon.fr>
2023-12-11 10:51:36 +0100

    logoDark configuré

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,19 +1,35 @@
-import siteMetadata from '@/data/siteMetadata'
+"use client";
+import siteMetadata from '@/data/siteMetadata.js'
 import headerNavLinks from '@/data/headerNavLinks'
 import Logo from '@/data/logoArt.svg'
 import Link from './Link'
 import MobileNav from './MobileNav'
-import ThemeSwitch from './ThemeSwitch'
 import SearchButton from './SearchButton'
+import { useTheme } from 'next-themes';
+import { useEffect, useState } from 'react'
 
 const Header = () => {
+  const { theme, setTheme, resolvedTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+  const [currentTheme, setCurrentTheme] = useState(theme);
+
+  useEffect(() => {
+    setMounted(true);
+    setCurrentTheme(theme);
+  }, [theme]);
+
+  useEffect(() => {
+    setCurrentTheme(resolvedTheme);
+  }, [resolvedTheme]);
+
   return (
     <header className="flex items-center justify-between py-10">
       <div>
         <Link href="/" aria-label={siteMetadata.headerTitle}>
           <div className="flex items-center justify-between">
             <div className="mr-3">
-              <Logo />
+            {(mounted && (currentTheme === 'dark')) &&  <img className="w-40 h-38" src = "/static/images/logoArtBlackNoBg.png" />}
+            {(mounted && (currentTheme === 'light')) &&  <Logo /> }
             </div>
             {typeof siteMetadata.headerTitle === 'string' ? (
               <div className="hidden h-6 text-2xl font-semibold sm:block">
@@ -25,6 +41,7 @@ const Header = () => {
           </div>
         </Link>
       </div>
+      
       <div className="flex items-center space-x-4 leading-5 sm:space-x-6">
         {headerNavLinks
           .filter((link) => link.href !== '/')
@@ -37,9 +54,30 @@ const Header = () => {
               {link.title}
             </Link>
           ))}
-        <ThemeSwitch />
+        <button
+          aria-label="Toggle Dark Mode"
+          onClick={() => setTheme(currentTheme === 'dark' ? 'light' : 'dark')}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            className="h-6 w-6 text-gray-900 dark:text-gray-100"
+          >
+            {mounted && (theme === 'dark' || resolvedTheme === 'dark') ? (
+              <path
+                fillRule="evenodd"
+                d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+                clipRule="evenodd"
+              />
+            ) : (
+              <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
+            )}
+          </svg>
+         </button>
         <MobileNav />
       </div>
+      
     </header>
   )
 }
