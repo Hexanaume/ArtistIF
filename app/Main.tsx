5ce2ff801fb6daf5c7d4f3132e069edf36a54261
Nour <mohamed-nour.eljadiri@insa-lyon.fr>
2023-12-13 09:15:39 +0100

    feat/La search bar monte quand y a des résultats

--- app/Main.tsx
+++ app/Main.tsx
@@ -1,27 +1,45 @@
+'use client';
 import Tag from '@/components/Tag';
 import Link from '@/components/Link';
 import SearchBar from '@/components/SearchBar';
 import siteMetadata from '@/data/siteMetadata';
 import { formatDate } from 'pliny/utils/formatDate';
 import NewsletterForm from 'pliny/ui/NewsletterForm';
+import { useState } from 'react';
 
 const MAX_DISPLAY = 5;
 
 export default function Home({ posts }) {
+    const [searchResults, setSearchResults] = useState([]);
+
+    const handleSearch = async (results) => {
+        setSearchResults(results);
+    };
+
     return (
         <>
-            <div className="divide-y divide-gray-200 dark:divide-gray-700">
-                <div className="flex flex-col items-center justify-center space-y-2 pb-8 pt-6 md:space-y-5">
-                    <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-                        Welcome to Artistif
-                    </h1>
-                    <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-                        {siteMetadata.description}
-                    </p>
+            {searchResults.length === 0 && (
+                <div className="divide-y divide-gray-200 dark:divide-gray-700">
+                    <div className="flex flex-col items-center justify-center space-y-2 pb-8 pt-6 md:space-y-5">
+                        <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+                            Welcome to Artistif
+                        </h1>
+                        <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+                            {siteMetadata.description}
+                        </p>
+                    </div>
                 </div>
-            </div>
-            <div>
-                <SearchBar />
+            )}
+            <div className="mt-8">
+                <div
+                    className={`transform ${
+                        searchResults.length > 0
+                            ? 'translate-y-0 transition-transform'
+                            : '-translate-y-10 transition-transform'
+                    }`}
+                >
+                    <SearchBar onSearch={handleSearch} />
+                </div>
             </div>
         </>
     );
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,52 +1,85 @@
-"use client";
-import SearchButton from "@/components/SearchButton";
-import React, {useState} from 'react';
-import { rechercher } from "../scripts/search.mjs";
+'use client';
+import SearchButton from '@/components/SearchButton';
+import React, { useState } from 'react';
+import { rechercher } from '../scripts/search.mjs';
 
-const SearchBar = () => {
-
-    const [searchTerm, setSearchTerm] = useState("");
+const SearchBar = ({ onSearch }) => {
+    const [searchTerm, setSearchTerm] = useState('');
     const [searchResults, setSearchResults] = useState([]);
     const handleChange = (event) => {
         event.preventDefault();
         setSearchTerm(event.target.value);
-        console.log("test : "+ searchTerm);
-    }
+        console.log('test : ' + searchTerm);
+    };
 
     const handleSearch = async (event) => {
         event.preventDefault();
 
-        console.log("searchTerm: "+searchTerm);
-        const res = await fetch(`http://localhost:3000/api/search?query=${encodeURIComponent(searchTerm)}`);
-        console.log("res: ", res);
+        console.log('searchTerm: ' + searchTerm);
+        const res = await fetch(
+            `http://localhost:3000/api/search?query=${encodeURIComponent(
+                searchTerm,
+            )}`,
+        );
+        console.log('res: ', res);
 
         const results = await res.json();
-        console.log("results: ", results);
+        console.log('results: ', results);
         setSearchResults(results);
-    }
+        onSearch(results);
+    };
 
     return (
         <>
             <div>
                 <form onSubmit={handleSearch}>
-                    <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+                    <label
+                        htmlFor="default-search"
+                        className="sr-only mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                        Search
+                    </label>
                     <div className="relative">
-                        <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                            <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                        <div className="pointer-events-none absolute inset-y-0 start-0 flex items-center ps-3">
+                            <svg
+                                className="h-4 w-4 text-gray-500 dark:text-gray-400"
+                                aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 20 20"
+                            >
+                                <path
+                                    stroke="currentColor"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                                />
                             </svg>
                         </div>
-                        <input onChange={handleChange} type="search" id="default-search" className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-gray-800 focus:border-gray-700 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-400 dark:focus:border-gray-400" placeholder="Search Mockups, Logos..." required/>
-                        <button onClick={handleSearch} type="button" className="text-white absolute end-2.5 bottom-2.5 bg-gray-800 hover:bg-grey-800 focus:ring-4 focus:outline-none focus:ring-gray-500 font-medium rounded-lg text-sm px-4 py-2 dark:bg-gray-900 dark:hover:bg-gray-700 dark:focus:ring-gray-600">Search</button>
+                        <input
+                            onChange={handleChange}
+                            type="search"
+                            id="default-search"
+                            className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-4 ps-10 text-sm text-gray-900 focus:border-gray-700 focus:ring-gray-800 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-gray-400 dark:focus:ring-gray-400"
+                            placeholder="Search Mockups, Logos..."
+                            required
+                        />
+                        <button
+                            onClick={handleSearch}
+                            type="button"
+                            className="hover:bg-grey-800 absolute bottom-2.5 end-2.5 rounded-lg bg-gray-800 px-4 py-2 text-sm font-medium text-white focus:outline-none focus:ring-4 focus:ring-gray-500 dark:bg-gray-900 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                        >
+                            Search
+                        </button>
                     </div>
                 </form>
             </div>
-
         </>
-);
-}
-function onSearch(){
-    console.log("test");
+    );
+};
+function onSearch() {
+    console.log('test');
 }
 
-export default SearchBar
+export default SearchBar;
