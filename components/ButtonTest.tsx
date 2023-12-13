c2713c9dea37cb680dd468e183c99b15e8d341d3
rafael-folgoas <rafael.folgoas@insa-lyon.frecho>
2023-12-13 09:34:37 +0100

    buttonTest

---
+++ components/ButtonTest.tsx
@@ -0,0 +1,42 @@
+/* eslint-disable prettier/prettier */
+'use client';
+import React, { useState } from 'react';
+import { rechercher } from '../scripts/search.mjs';
+
+const SearchBar = () => {
+    const [searchTerm, setSearchTerm] = useState('');
+    const [searchResults, setSearchResults] = useState([]);
+
+    const handleTest = async (event) => {
+        event.preventDefault();
+        const testString = 'test';
+        console.log('searchTerm: ' + searchTerm);
+        const res = await fetch(
+            `http://localhost:3000/api/search?query=${testString}&type=rechercher`,
+        );
+        console.log('res: ', res);
+
+        const results = await res.json();
+        console.log('results: ', results);
+        setSearchResults(results);
+    };
+
+    return (
+        <>
+            <div>
+                <button
+                    onClick={handleTest}
+                    type="button"
+                    className="hover:bg-grey-800 rounded-lg bg-gray-800 px-4 py-2 text-sm font-medium text-white focus:outline-none focus:ring-4 focus:ring-gray-500 dark:bg-gray-900 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                >
+                    Test
+                </button>
+            </div>
+        </>
+    );
+};
+function onSearch() {
+    console.log('test');
+}
+
+export default SearchBar;
