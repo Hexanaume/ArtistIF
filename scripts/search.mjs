5b57797c41e8722907bb1f6e4fdd48577ea4b734
rafael-folgoas <rafael.folgoas@insa-lyon.frecho>
2023-12-19 14:55:46 +0100

    search changes

--- scripts/search.mjs
+++ scripts/search.mjs
@@ -1,3 +1,5 @@
+/* eslint-disable prettier/prettier */
+
 function addDbpediaPrefixes(requestString) {
   // append the prefixes to the request string
   const prefixes = 'PREFIX owl: <http://www.w3.org/2002/07/owl#> \n' +
@@ -53,6 +55,22 @@ function requestArtists(NomArtists){
   return requestString;
 }
 
+
+function requestInfosArtists(urlArtist){
+  let requestString;
+  requestString = `SELECT distinct ?artist ?label ?abstract  WHERE {
+
+    ?artist dbo:wikiPageLength ?pageLength .
+    ?artist a dbo:Artist.
+    ?artist rdfs:label ?label .
+    ?artist dbo:abstract ?abstract .
+    filter contains(?artist,"${urlArtist}").
+  
+  } ORDER BY desc(?pageLength)`;
+
+  return requestString;
+}
+
 export async function rechercher(inputString) {
 
   // Ajout des préfixes
@@ -62,18 +80,78 @@ export async function rechercher(inputString) {
   console.log("requestString",requestString);
 
   const url = "https://dbpedia.org/sparql?query=" + encodeURIComponent(requestString) + "&format=json";
-
+  console.log(url);
 
   try {
     const response = await fetch(url);
-
-    const responseJson = await response.json();
-    console.log(responseJson.results.bindings);
-    return responseJson.results.bindings;
+    return await response.json();
   } catch (error) {
     return console.log("Erreur : " + error);
   }
 
 }
+export async function getInfosArtist(urlArtist){
+  // Ajout des préfixes
+  let requestString;
+  requestString = addDbpediaPrefixes(requestInfosArtists(urlArtist));
 
+  console.log("requestString",requestString);
+
+  const url = "https://dbpedia.org/sparql?query=" + encodeURIComponent(requestString) + "&format=json";
+  console.log(url);
+
+  try {
+    const response = await fetch(url);
+    return await response.json();
+  } catch (error) {
+    return console.log("Erreur : " + error);
+  }
+
+}
 // Affichage des résultats dans un tableau
+function afficherResultats(data)
+{
+  // Tableau pour mémoriser l'ordre des variables ; sans doute pas nécessaire
+  // pour vos applications, c'est juste pour la démo sous forme de tableau
+  var index = [];
+
+  var contenuTableau = "<tr>";
+
+  data.head.vars.forEach((v, i) => {
+    contenuTableau += "<th>" + v + "</th>";
+    index.push(v);
+  });
+
+  data.results.bindings.forEach(r => {
+    contenuTableau += "<tr>";
+
+    index.forEach(v => {
+
+      if (r[v])
+      {
+        if (r[v].type === "uri")
+        {
+          contenuTableau += "<td><a href='" + r[v].value + "' target='_blank'>" + r[v].value + "</a></td>";
+        }
+        else {
+          contenuTableau += "<td>" + r[v].value + "</td>";
+        }
+      }
+      else
+      {
+        contenuTableau += "<td></td>";
+      }
+
+    });
+
+
+    contenuTableau += "</tr>";
+  });
+
+
+  contenuTableau += "</tr>";
+
+  document.getElementById("resultats").innerHTML = contenuTableau;
+
+}
+
