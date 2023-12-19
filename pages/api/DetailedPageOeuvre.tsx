85cb6039c539481279b64cb9f97631f7c35dba65
rafael-folgoas <rafael.folgoas@insa-lyon.frecho>
2023-12-19 16:22:41 +0100

    detailled page oeuvre et movement

--- pages/api/DetailedPageOeuvre.tsx
+++ pages/api/DetailedPageOeuvre.tsx
@@ -2,9 +2,9 @@ import SearchButton from '@/components/SearchButton';
 import React, { useState, useEffect } from 'react';
 import { rechercher } from '../scripts/search.mjs';
 import Card, { SearchResult } from '@/components/Card';
-import DetailledCard from '@/components/DetailledCard';
+import DetailledOeuvre from '@/components/DetailledArt';
 
-const DetailedPageO = ({ idPage }) => {
+const DetailedPageOeuvre = ({ idPage }) => {
     const [searchTerm, setSearchTerm] = useState('');
     const [searchResults, setSearchResults] = useState([]);
 
@@ -27,11 +27,10 @@ const DetailedPageO = ({ idPage }) => {
 
     useEffect(() => {
         handleSearch();
-    }, [idPage]); 
+    }, [idPage]);
 
     return (
         <>
-
             <div className="mt-8">
                 <div
                     className={`transform ${
@@ -39,25 +38,23 @@ const DetailedPageO = ({ idPage }) => {
                             ? '-translate-y-10 transition-transform'
                             : 'transition-transform'
                     }`}
-                >
-                </div>
+                ></div>
             </div>
 
             <div className="flex flex-row flex-wrap">
                 {searchResults.length > 0 &&
                     searchResults.map((result: SearchResult, index) => {
                         return (
-                            <DetailledCard
+                            <DetailledOeuvre
+                                key={index} // Ajout de la clé unique ici
                                 title={result.name.value}
                                 imgSrc={result.picture.value}
-                                author={result.name.value}
-                                date={"test"}
-                                comment={result.abstract.value}
-                                era={"test"}
-                                storage={"test"}
-                                technique={"test"}
-                                price={"test"}
-
+                                author={result.artist.value}
+                                date={result.date.value}
+                                era={result.era.value}
+                                storage={result.storage.value}
+                                price={result.price.value}
+                                description={result.abstract.value}
                             />
                         );
                     })}
@@ -66,4 +63,4 @@ const DetailedPageO = ({ idPage }) => {
     );
 };
 
-export default DetailedPageO;
+export default DetailedPageOeuvre;
